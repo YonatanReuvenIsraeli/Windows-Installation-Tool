241450a3828e6963fb905ed75b3e52593e37923e
Yonatan Israeli <yonirisraeli@gmail.com>
2025-01-06 18:44:56 -0500

    Update Windows Installation Tool.bat

--- Windows Installation Tool.bat
+++ Windows Installation Tool.bat
@@ -2,7 +2,7 @@
 setlocal
 title Windows Installation Tool
 echo Program Name: Windows Installation Tool
-echo Version: 5.0.16
+echo Version: 5.0.17
 echo License: GNU General Public License v3.0
 echo Developer: @YonatanReuvenIsraeli
 echo GitHub: https://github.com/YonatanReuvenIsraeli
@@ -925,7 +925,7 @@ echo.
 echo Creating recovery partition.
 md "%Recovery%\Recovery\WindowsRE" > nul 2>&1
 copy "%NTFS%\Windows\System32\Recovery\winre.wim" "%Recovery%\Recovery\WindowsRE\winre.wim" /y /v > nul 2>&1
-"%NTFS%\System32\ReAgentc.exe" /setreimage /path "%Recovery%\Recovery\WindowsRE" /target "%NTFS%\Windows" > nul 2>&1
+"%NTFS%\Windows\System32\ReAgentc.exe" /setreimage /path "%Recovery%\Recovery\WindowsRE" /target "%NTFS%\Windows" > nul 2>&1
 if not "%errorlevel%"=="0" goto "RecoveryError"
 (echo sel vol %Recovery%) > "%cd%\diskpart.txt"
 (echo remove letter=%Recovery%) >> "%cd%\diskpart.txt"
